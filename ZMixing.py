4629266364e194f26380fbf0265b634dbf27072d
Jason Powell <jjp2162@columbia.edu>
2018-03-25 10:28:11 -0400

    Add files via upload

---
+++ ZMixing.py
@@ -0,0 +1,154 @@
+# Jake Mayeux
+# July 2017
+# mixing-v2.py
+
+import re
+import sys
+import getopt
+from os import walk
+
+absolute = -1
+
+infile = 'ZMixingIn.gcode'
+
+outfile = 'ZMixingOut.gcode'
+
+colorfile = 'color-codes.txt'
+
+maxZ = float(-1.0)
+
+zhop = float(1.0) # needed to properly detect layer changes
+
+zpos = float(0.0)
+
+#lastz = float(-1.0)
+
+numColors = -1 # -1 to use all colors from the color code file
+
+numExtruders = 16 # number of virtual extruders our printer can handle
+
+opts, extraparams = getopt.getopt(
+   sys.argv[1:],
+   'z:f:o:',
+   ['zhop', 'infile', 'outfile'])
+for o, p in opts:
+   if o in ['-z', '--zhop']:
+      zhop = p
+   elif o in ['-f', '--infile']:
+      infile = p
+   elif o in ['-o', '--outfile']:
+      outfile = p
+
+inf = open(infile, 'r')
+cof = open(colorfile, 'r')
+ouf = open(outfile, 'w')
+
+lines = inf.readlines()
+colors = cof.readlines()
+
+# change the mixing ratios of the virtual extruders to colors defined in the color-codes file
+def setColors(start, length):
+    s = 0
+    i = 0
+    for l in colors[start:start+length]:
+        for c in l:
+            if c != '\n':
+                ouf.write('M163 S'+str(s)+' P'+c+'\n')
+                #print('M163 S'+str(s)+' P'+c)
+                s += 1
+        s = 0
+        ouf.write('M164 S'+str(i)+'\n\n')
+        #print('M164 S'+str(i)+'\n')
+        i += 1
+
+
+#setColors(32,16)
+#sys.exit()
+
+def getValue(line, key, default=None):
+    if (key not in line) or (';' in line and line.find(key) > line.find(';')):
+        return default
+    sub_part = line[line.find(key) + 1:]
+    m = re.search('^[0-9]+\.?[0-9]*', sub_part)
+    if m is None:
+        return default
+    try:
+        return float(m.group(0))
+    except ValueError:
+        return default
+
+# iterate through lines from last to firstG92
+for x in reversed(range(len(lines))):
+   l = lines[x]
+   i = l.find(' ')
+   if (i == -1):
+      gc = l
+   else:
+      gc = l[0:i]
+   
+   # remove comments
+   if (gc.find(';') != -1):
+      gc = gc[:gc.find(';')]
+
+   if (gc == 'G1'):
+      # if we got a Z, its the last z, so its the max
+      maxZ = getValue(l, 'Z', -1)
+      if (maxZ != -1):
+         break
+
+if numColors == -1:
+    numSwaps = len(colors)
+else:
+    numColors = numExtruders
+
+swapHeight = maxZ / numSwaps
+nextSwap = swapHeight
+print(maxZ)
+print(swapHeight)
+print(numSwaps)
+
+numResets = 0
+ct = 0 # current T
+firstG92 = True
+firstG28 = True
+#ouf.write('T'+str(ct)+'\n')
+for l in lines:
+   i = l.find(' ')
+   if (i == -1):
+      gc = l
+   else:
+      gc = l[0:i]
+   
+   # remove comments
+   if (gc.find(';') != -1):
+      gc = gc[:gc.find(';')]
+
+   if (gc == 'G1'):
+      # check if Z has changed, check if it is a zhop movement
+      newz = getValue(l, 'Z', -1)
+      #print(newz)
+      if (newz != -1 and newz - zpos < zhop - 0.01 and newz != zpos):
+         if (zpos == 0):
+            zpos = newz
+         elif (zpos > 0):
+            zpos = newz
+            # new layer here
+            if (zpos >= nextSwap):
+               nextSwap += swapHeight
+               #print(nextSwap)
+               if ct == -1:
+                  numResets += 1
+                  setColors(numExtruders*numResets, numExtruders)        
+               ct += 1
+               ouf.write('T'+str(ct)+'\n')
+               if ct >= numExtruders-1:
+                  ct = -1
+
+   # print firstG92 Tn after first G92
+   ouf.write(l)
+   if (gc == 'G92' and firstG92):
+      firstG92 = False
+      ouf.write('T0\n')
+   if (gc == 'G28' and firstG28):
+      firstG28 = False
+      setColors(numExtruders*numResets, numExtruders)        
--- purge-tower.py
+++ purge-tower.py
@@ -5,9 +5,12 @@
 import re
 import sys
 import getopt
+import math
 from os import walk
 
 absolute = -1
+
+genTowers = True # whether to auto gen the towers or read a template from a file
 
 towerfile = 'tower.gcode'
 
@@ -21,6 +24,8 @@ towers = []
 
 towersPath = 'PurgeTXTs'
 
+nozzleDiam = float(0.4) # for generating circular
+
 numTowers = 0
 
 zhop = float(1.0) # needed to properly detect layer changes
@@ -30,6 +35,8 @@ zpos = float(0.0)
 tn = -1
 
 data = {}
+
+lastz = float(-1.0) # last z pos to draw purge towers at
 
 ltn = -1 # last Tn
 maxSwaps = 0
@@ -52,6 +59,18 @@ ouf = open(outfile, 'w')
 
 lines = inf.readlines()
 
+
+def genTower(x, y, radius):
+   for r = radius; r <= nozzleDiam*2; r -= nozzleDiam:
+      ret = ['G1 X'+str(x+r)+' Y'+str(y)]
+      ret.append('G2 X'+str(x+r)+' Y'+str(y)+' I'+str(x-r)+' J'+str(y))
+
+
+def genTowers(qty, radius):
+   global towers
+   #for i in range(qty):
+      #towers.append(genTower(radius))
+
 def getValue(line, key, default=None):
     if (key not in line) or (';' in line and line.find(key) > line.find(';')):
         return default
@@ -64,13 +83,9 @@ def getValue(line, key, default=None):
     except ValueError:
         return default
 
-for (dirpath, dirnames, filenames) in walk(towersPath):
-   towerFiles.extend(filenames)
-   break
 
 # find max # of times were going to change colors
 for l in lines:
-   ouf.write(l)
    i = l.find(' ')
    if (i == -1):
       gc = l
@@ -89,26 +104,32 @@ for l in lines:
             zpos = newz
          else:
             # new layer here
-            #ouf.write('TNEW '+str(zpos)+' '+str(newz)+'\n')
             print('new layer')
             zpos = newz
             swaps = 0
-            #print('reset')
    elif (gc[0:1] == 'T'):
       tn = int(gc[1:])
       if (ltn == -1):
          ltn = tn
+         lastz = zpos
       elif (tn != ltn):
-         print('new swap', ltn, tn)
+         #print('new swap', ltn, tn)
+         lastz = zpos
          ltn = tn
          swaps += 1
          #print('swap', swaps, l)
          if (swaps > maxSwaps):
             maxSwaps = swaps
 
- 
-for f in sorted(towerFiles):
-   towers.append(open(towersPath+'/'+f, 'r').readlines())
+if genTowers:
+   genTowers()
+else:
+   for (dirpath, dirnames, filenames) in walk(towersPath):
+      towerFiles.extend(filenames)
+      break
+   for f in sorted(towerFiles):
+      towers.append(open(towersPath+'/'+f, 'r').readlines())
+
 
 print('maxSwaps', maxSwaps)
 
@@ -118,13 +139,19 @@ def drawNextTower():
       print('not enough towers, skipping purge')
    else:
       for l in towers[swaps]:
+         # dont write lines that move the Z or reset the extruder
+         #if (l.find('Z') == -1 and l.find('G92') == -1):
          if (l.find('Z') == -1):
             ouf.write(l)
    swaps += 1
 
+print('lastz: ', lastz)
+zpos = float(0.0)
 
 for l in lines:
-   ouf.write(l)
+   if (zpos > lastz):
+      ouf.write(l)
+      continue
    i = l.find(' ')
    if (i == -1):
       gc = l
@@ -141,13 +168,10 @@ for l in lines:
       if (newz != -1 and newz - zpos < zhop - 0.01 and newz != zpos):
          if (zpos == 0):
             zpos = newz
-         else:
+         elif (zpos > 0):
             # new layer here
-            #ouf.write('TNEW '+str(zpos)+' '+str(newz)+'\n')
             for i in list(range(maxSwaps-swaps)):
                drawNextTower()
-               #for l in towers[i]:
-                  #ouf.write(';asd\n')
             zpos = newz
             swaps = 0
    elif (gc[0:1] == 'T'):
@@ -157,22 +181,6 @@ for l in lines:
       elif (tn != ltn):
          # swap here
          ltn = tn
-         drawNextTower()
-         #print('swap', swaps, l)
-
-#for l in lines:
-#   break
-#   i = l.find(' ')
-#   if (i == -1):
-#      gc = l
-#   else:
-#      gc = l[0:i]
-#
-#   if (gc.find(';') != -1):
-#      gc = gc[:gc.find(';')]
-
-   #if (gc[0:1] == 'T' and gc[1:] not in tnList):
-      #tnList[gc] = False
-
-
-
+         if (zpos > 0):
+            drawNextTower()
+   ouf.write(l)
